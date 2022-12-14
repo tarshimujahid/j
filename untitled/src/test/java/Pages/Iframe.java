ffb3b3e8c1ceefa476f936e9bddd3bf1258f5678
MTarshi <106284081+MTarshi@users.noreply.github.com>
2022-12-14 13:51:41 -0500

    Initial commit2

---
+++ untitled/src/test/java/Pages/Iframe.java
@@ -0,0 +1,31 @@
+package Pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Iframe {
+
+    public static String url = "http://localhost:7080/iframe";
+
+@Test
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        driver.switchTo().frame(0);
+
+        WebElement textBox = driver.findElement(By.id("tinymce"));
+        textBox.clear();
+        textBox.sendKeys("Good Morning");
+
+        driver.switchTo().defaultContent();
+
+        driver.quit();
+    }
+}
